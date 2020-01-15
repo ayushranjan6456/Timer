8adca8669c80052c09835b0096f84c996e121a26
Ayush Ranjan <ayushranjan6456@gmail.com>
2020-01-16 01:05:07 +0530

    Timer

--- app.js
+++ app.js
@@ -1,45 +1,47 @@
 function start(){  
-
-  
-document.getElementById("startBtn").onclick = ""; 
-var clickBtn = document.getElementById("startBtn"); 
+  var clickBtn = document.getElementById("startBtn"); 
   clickBtn.classList.toggle("clicked");
+  window.scrollTo(0,document.body.scrollHeight);
 
-var h = parseInt(document.getElementById("inputHour").value, 0);
-var m = parseInt(document.getElementById("inputMinute").value, 0);
-var s = parseInt(document.getElementById("inputSecond").value, 0);
+  var d = (document.getElementById("inputDay").value);
+  var h = parseInt(document.getElementById("inputHour").value, 0);
+  var m = parseInt(document.getElementById("inputMinute").value, 0);
+  var s = parseInt(document.getElementById("inputSecond").value, 0);
+
+  var countDownDate = new Date(d).getTime();
+
+  if(s>60){
+    document.getElementById("inputMinute").value=Math.floor(s/60);
+    document.getElementById("inputSecond").value=s%60;
+  }
 
 
-if(s>60){
-  document.getElementById("inputMinute").value=Math.floor(s/60);
-  document.getElementById("inputSecond").value=s%60;
-}
+  if(d === ''){
+    alert("Enter the date!");
+    location.reload();
+  }
 
-
-if(h == 0 && m == 0 && s == 0){
-  alert("Cannot Start Timer with 0...");
-  location.reload();
-}
-
-if(h < 0 || m < 0 || s < 0){
-  alert("Enter positive number. ");
-  location.reload();
-}
-
-var current = ((h * 3600) + (m * 60) + s - 1);                                              
-var hours=0,minutes=0,seconds=0;
-var Timer = setInterval(function(){ 
-    hours = Math.floor(current /(60 * 60)); 
-    minutes = Math.floor((current % (60 * 60)) / (60)); 
-    seconds = Math.floor((current % (60))); 
-    document.getElementById("hour").innerHTML = hours; 
-    document.getElementById("minute").innerHTML = minutes ;  
-    document.getElementById("second").innerHTML = seconds;
-    current-=1;
-  if(current < 0){
-    clearInterval(Timer);
-    document.getElementById("timer").innerHTML = ""; 
-    document.getElementById("demo").innerHTML = "Time Up";
-} 
-  }, 1000);
+  if(h < 0 || m < 0 || s < 0 || d < 0){
+    alert("Enter positive number. ");
+    location.reload();
+  }
+  
+  var Timer = setInterval(function(){ 
+    var now = new Date().getTime();
+    var distance = countDownDate-now-(1000*60*60*5)-(1000*60*30)+(1000*60*m)+(1000*60*60*h)+(1000*s); 
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000); 
+      document.getElementById("day").innerHTML = days; 
+      document.getElementById("hour").innerHTML = hours; 
+      document.getElementById("minute").innerHTML = minutes ;  
+      document.getElementById("second").innerHTML = seconds;
+      distance-=1;
+    if(distance < 0){
+      clearInterval(Timer);
+      document.getElementById("timer").innerHTML = ""; 
+      document.getElementById("demo").innerHTML = "Time Up";
+  } 
+    }, 1000);
 }
